adbacc48b87fea4985379f03e0437ffbea9b2136
Pandasky <745098586@qq.com>
2019-06-27 17:37:48 +0800

    190627学习内容

--- 更新英雄位置.py
+++ 更新英雄位置.py
@@ -11,18 +11,26 @@ screen.blit(bg, (0,0))
 
 #绘制英雄的飞机
 hero = pygame.image.load("./image/me1.png")
-screen.blit(hero, (200, 500))
+screen.blit(hero, (150, 300))
 
 pygame.display.update()
 
 #创建时钟对象
 clock = pygame.time.Clock()
 
-#游戏循环
-i = 0
+#1. 定义rect记录飞机的初始位置
+hero_rect = pygame.Rect(150, 300, 102, 126)
 
+#游戏循环
 while True:
-    pass
+    clock.tick(60)
+
+    hero_rect.y -= 5
+
+    screen.blit(bg, (0, 0))
+    screen.blit(hero, hero_rect)
+
+    pygame.display.update()
 
 
 pygame.quit()
